525240af51aeedd3661fa6f451a1a34967ee4b7b
finless88 <nazmul96@gmail.com>
2023-04-11 01:34:40 +0600

    Applied job link is working matching data previously stored in localstorage'

--- src/component/appliedJobs/AppliedJobs.jsx
+++ src/component/appliedJobs/AppliedJobs.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 import { getShoppingCart } from "../../component/utilities/localStore.js";
 import AppliedJobloader from "../loader/AppliedJobloader.js";
+import { useState, useEffect } from "react";
 const AppliedJobs = () => {
+  const [appliedJob, setAppliedJob] = useState([]);
   const featuredata = useLoaderData();
 
   // to know the applied job we need to know their id
-  const obj = getShoppingCart();
-  for (let JobId in obj) {
-    console.log("JobId", JobId);
-  }
-  console.log("data", data);
+  useEffect(() => {
+    const obj = getShoppingCart();
+    const savedcart = [];
+    for (let JobId in obj) {
+      console.log("JobId", JobId);
+      const applied = featuredata.find((item) => item.id === JobId);
+      savedcart.push(applied);
+      setAppliedJob(savedcart);
+    }
+  }, [featuredata]);
+
+  console.log("data", featuredata);
   return (
     <div>
       <h6>Applied</h6>
       {/* <h4>{appliedJob.map((item) => console.log(item))}</h4> */}
+
+      {appliedJob.length}
     </div>
   );
 };
