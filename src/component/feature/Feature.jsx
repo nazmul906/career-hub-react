5702949ae54c12b6cef9023184b37d3b3781fb4d
finless88 <nazmul96@gmail.com>
2023-04-12 04:41:12 +0600

    Home page is designed

--- src/component/feature/Feature.jsx
+++ src/component/feature/Feature.jsx
@@ -7,21 +7,37 @@ import DisplayFeature from "../display/DisplayFeature";
 import { Link } from "react-router-dom";
 const Feature = ({ feature }) => {
   // console.log(feature);
-  const { id, company_name, name, remote_or_onsite, location } = feature;
+  const {
+    id,
+    job_title,
+    company_logo,
+    company_name,
+    name,
+    remote_or_onsite,
+    location,
+    salary,
+  } = feature;
   return (
     <div className="feature">
       <h4>{company_name}</h4>
-      <Link to={`/viewdetails/${id}`}>
-        <button>View Details</button>
-      </Link>
+      <img style={{ width: "100px", height: "100px" }} src={company_logo}></img>
+      <p>{job_title}</p>
+      <p>{company_name}</p>
+      <p>
+        <button>{remote_or_onsite}</button> <button>Full time</button>{" "}
+        <span></span>
+      </p>
+      <p>
+        {location} <span className="ms-2">{salary}</span>
+      </p>
+      {/* <div className="card-body"> */}
 
-      <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
+      {/* <div className="card-actions "> */}
+      <Link to={`/viewdetails/${id}`}>
+        <button className="btn btn-primary">View Details</button>
+      </Link>
+      {/* </div> */}
+      {/* </div> */}
     </div>
   );
 };
--- src/component/jobDetails/JobDetails.jsx
+++ src/component/jobDetails/JobDetails.jsx
@@ -3,7 +3,17 @@ import { useParams } from "react-router-dom";
 import { addToDb } from "../utilities/localStore";
 import { useState, useEffect } from "react";
 const JobDetails = ({ details }) => {
-  const { id, job_title, company_name, remote_or_onsite } = details;
+  const {
+    educational_requirements,
+    salary,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    job_description,
+    job_responsibility,
+    experiences,
+    contact_information,
+  } = details;
   // console.log(details);
   const handleClick = (id) => {
     console.log("clicked", id);
@@ -11,15 +21,45 @@ const JobDetails = ({ details }) => {
 
     // const prevlocalstorage
   };
+  console.log(Object.keys(contact_information));
   return (
     <div>
-      <div>
+      {/* <div>
         <h6>ID: {id}</h6>
         <h6>Company Name: {company_name}</h6>
         <h6>Job Title: {job_title}</h6>
         <h6>Remote or Onsite: {remote_or_onsite}</h6>
-      </div>
+      </div> */}
       <button onClick={() => handleClick(id)}>Apply job</button>
+      {/*  */}
+      <div className="card card-side bg-base-100 shadow-xl">
+        {/* <figure>
+          
+        </figure> */}
+        <div>
+          <p>{job_description}</p>
+          <p>{job_responsibility}</p>
+          <p>{educational_requirements}</p>
+          <h6>{experiences}</h6>
+        </div>
+        <div className="card-body">
+          <div>
+            <h5 className="card-title">Job details</h5>
+            <p>{salary}</p>
+            <p>{job_title}</p>
+          </div>
+          <div>
+            <h5 className="card-title">Contact</h5>
+
+            <p>{contact_information.phone}</p>
+            <p>{contact_information.email}</p>
+          </div>
+
+          {/* <div className="card-actions ">
+            <button className="btn btn-primary">Watch</button>
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 };
